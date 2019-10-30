ffbb0ab7cfc1d9772d9d847dc8bccbdbd3578f9e
Jeremy Cantrell <jmcantrell@gmail.com>
2019-10-30 00:39:15 -0500

    Style changes and change license

--- setup.py
+++ setup.py
@@ -3,35 +3,42 @@
 from setuptools import setup
 
 setup(
-        name='DateUtils',
-        version='0.6.5',
-        description='Various utilities for working with datetime objects.',
-        author='Jeremy Cantrell',
-        author_email='jmcantrell@gmail.com',
-        zip_safe=False,
-        include_package_data=True,
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            ],
-        install_requires=[
-            'argparse',
-            'future',
-            'python-dateutil',
-            'pytz'
-            ],
-        entry_points={
-            'console_scripts': [
-                'dateadd = dateutils.dateadd:main',
-                'datediff = dateutils.datediff:main',
-                ],
-            },
-        packages=[
-            'dateutils',
-            ],
-        )
+
+    name='dateutils',
+    version='0.6.7',
+
+    description='Various utilities for working with date and datetime objects',
+
+    author='Jeremy Cantrell',
+    author_email='jmcantrell@gmail.com',
+
+    zip_safe=False,
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+    ],
+
+    install_requires=[
+        'argparse',
+        'python-dateutil',
+        'pytz'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'dateadd = dateutils.dateadd:main',
+            'datediff = dateutils.datediff:main',
+        ],
+    },
+
+    packages=[
+        'dateutils',
+    ],
+
+)
